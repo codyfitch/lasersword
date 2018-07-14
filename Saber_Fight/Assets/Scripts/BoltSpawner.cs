3037a032e54ec6bee2d4409837f143f020102dee
Cody Fitch <35245717+codyfitch@users.noreply.github.com>
2018-07-13 20:04:10 -0600

    Add files via upload

---
+++ Saber_Fight/Assets/Scripts/BoltSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltSpawner : MonoBehaviour {
+
+    public GameObject bolt;
+    public Transform boltSpawn;
+    public int boltSpeed;
+    public Vector3 TargetPosition;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Spawner();
+        }
+    }
+
+	void Spawner()
+    {
+        var laserBolt = (GameObject)Instantiate(bolt, boltSpawn.position, boltSpawn.rotation);
+        laserBolt.GetComponent<Rigidbody>().velocity = laserBolt.transform.forward * boltSpeed;
+
+    }
+}
